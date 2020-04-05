26d5481f7f1f03279d8f297842d421faaa650bcd
Masayuki Kimura <kimura@sifive-china.com>
2020-04-05 17:05:50 +0900

    Add Relocation Informaiton for MYRISCVX

--- llvm/lib/Object/ELF.cpp
+++ llvm/lib/Object/ELF.cpp
@@ -20,9 +20,11 @@ using namespace object;
 
 #define ELF_RELOC(name, value) STRINGIFY_ENUM_CASE(ELF, name)
 
+// @{ ELF_cpp_EM_MYRISCVX
 StringRef llvm::object::getELFRelocationTypeName(uint32_t Machine,
                                                  uint32_t Type) {
   switch (Machine) {
+// @{ ELF_cpp_EM_MYRISCVX ...
   case ELF::EM_X86_64:
     switch (Type) {
 #include "llvm/BinaryFormat/ELFRelocs/x86_64.def"
@@ -146,6 +148,15 @@ StringRef llvm::object::getELFRelocationTypeName(uint32_t Machine,
       break;
     }
     break;
+// @} ELF_cpp_EM_MYRISCVX ...
+  case ELF::EM_MYRISCVX:
+    switch (Type) {
+#include "llvm/BinaryFormat/ELFRelocs/MYRISCVX.def"
+    default:
+      break;
+    }
+    break;
+    // @} ELF_cpp_EM_MYRISCVX
   default:
     break;
   }
