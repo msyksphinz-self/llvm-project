75867987b0301f70336a8ae7d920e4222ddcc95a
msyksphinz <msyksphinz.dev@gmail.com>
2020-05-01 16:11:13 +0900

    Implement eliminateFrameIndex

--- llvm/lib/Target/MYRISCVX/MYRISCVXRegisterInfo.cpp
+++ llvm/lib/Target/MYRISCVX/MYRISCVXRegisterInfo.cpp
@@ -77,6 +77,74 @@ getReservedRegs(const MachineFunction &MF) const {
 void MYRISCVXRegisterInfo::
 eliminateFrameIndex(MachineBasicBlock::iterator II, int SPAdj,
                     unsigned FIOperandNum, RegScavenger *RS) const {
+  MachineInstr &MI = *II;
+  MachineFunction &MF = *MI.getParent()->getParent();
+  MachineFrameInfo MFI = MF.getFrameInfo();
+
+  // @{eliminateFrameIndex_FindFrameIndex
+  unsigned i = 0;
+  while (!MI.getOperand(i).isFI()) {
+    ++i;
+    assert(i < MI.getNumOperands() &&
+           "Instr doesn't have FrameIndex operand!");
+  }
+  // @}eliminateFrameIndex_FindFrameIndex
+
+  LLVM_DEBUG(errs() << "\nFunction : " << MF.getFunction().getName() << "\n";
+             errs() << "<--------->\n" << MI);
+
+  // @{eliminateFrameIndex_getOffset
+  int FrameIndex = MI.getOperand(i).getIndex();
+  uint64_t stackSize = MF.getFrameInfo().getStackSize();
+  int64_t spOffset = MF.getFrameInfo().getObjectOffset(FrameIndex);
+  // @}eliminateFrameIndex_getOffset
+
+  LLVM_DEBUG(errs() << "FrameIndex : " << FrameIndex << "\n"
+             << "spOffset   : " << spOffset << "\n"
+             << "stackSize  : " << stackSize << "\n");
+
+  // const std::vector<CalleeSavedInfo> &CSI = MFI.getCalleeSavedInfo();
+  // int MinCSFI = 0;
+  // int MaxCSFI = -1;
+  //
+  // if (CSI.size()) {
+  //   MinCSFI = CSI[0].getFrameIdx();
+  //   MaxCSFI = CSI[CSI.size() - 1].getFrameIdx();
+  // }
+
+  // The following stack frame objects are always referenced relative to $sp:
+  //  1. Outgoing arguments.
+  //  2. Pointer to dynamically allocated stack space.
+  //  3. Locations for callee-saved registers.
+  // Everything else is referenced relative to whatever register
+  // getFrameRegister() returns.
+  unsigned FrameReg = MYRISCVX::SP;
+
+  // Calculate final offset.
+  // - There is no need to change the offset if the frame object is one of the
+  //   following: an outgoing argument, pointer to a dynamically allocated
+  //   stack space or a $gp restore location,
+  // - If the frame object is any of the following, its offset must be adjusted
+  //   by adding the size of the stack:
+  //   incoming argument, callee-saved register location or local variable.
+  // @{eliminateFrameIndex_calcOffset
+  int64_t Offset;
+  Offset  = spOffset + (int64_t)stackSize;
+  Offset += MI.getOperand(i+1).getImm();
+  // @}eliminateFrameIndex_calcOffset
+
+  LLVM_DEBUG(errs() << "Offset     : " << Offset << "\n" << "<--------->\n");
+
+  // If MI is not a debug value, make sure Offset fits in the 12-bit immediate
+  // field.
+  if (!MI.isDebugValue() && !isInt<12>(Offset)) {
+	assert("(!MI.isDebugValue() && !isInt<16>(Offset))");
+  }
+
+  // @{eliminateFrameIndex_changeMI
+  MI.getOperand(i+0).ChangeToRegister(FrameReg, false);
+  MI.getOperand(i+1).ChangeToImmediate(Offset);
+  // @}eliminateFrameIndex_changeMI
 }
 // @}MYRISCVXRegisterInfo_eliminateFrameIndex
 
