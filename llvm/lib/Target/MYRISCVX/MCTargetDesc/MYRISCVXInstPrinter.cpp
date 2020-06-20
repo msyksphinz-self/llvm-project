03432f782795d326fa3c51a497995aab9bf9e62d
msyksphinz <msyksphinz.dev@gmail.com>
2020-06-20 15:36:04 +0900

    Add MYRISCVXInstPrinter

---
+++ llvm/lib/Target/MYRISCVX/MCTargetDesc/MYRISCVXInstPrinter.cpp
@@ -0,0 +1,90 @@
+//===-- MYRISCVXInstPrinter.cpp - Convert MYRISCVX MCInst to assembly syntax ------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===------------------------------------------------------------------------------===//
+//
+// This class prints an MYRISCVX MCInst to a .s file.
+//
+//===------------------------------------------------------------------------------===//
+
+#include "MYRISCVXInstPrinter.h"
+
+#include "MYRISCVXInstrInfo.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/MC/MCExpr.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCInstrInfo.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+#define PRINT_ALIAS_INSTR
+#include "MYRISCVXGenAsmWriter.inc"
+
+void MYRISCVXInstPrinter::printRegName(raw_ostream &OS, unsigned RegNo) const {
+  //- getRegisterName(RegNo) defined in MYRISCVXGenAsmWriter.inc which indicate in
+  //   MYRISCVX.td.
+  OS << StringRef(getRegisterName(RegNo)).lower();
+}
+
+// @{ MYRISCVXInstPrinter_cpp_printInst
+void MYRISCVXInstPrinter::printInst(const MCInst *MI, uint64_t Address,
+                                    StringRef Annot, const MCSubtargetInfo &STI,
+                                    raw_ostream &O) {
+  // Try to print any aliases first.
+  if (!printAliasInstr(MI, O))
+    //- printInstruction(MI, O) defined in MYRISCVXGenAsmWriter.inc which came from
+    //   MYRISCVX.td indicate.
+    printInstruction(MI, Address, O);
+  printAnnotation(O, Annot);
+}
+// @} MYRISCVXInstPrinter_cpp_printInst
+
+
+// @{ MYRISCVXInstPrinter_cpp_printOperand
+void MYRISCVXInstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
+                                       raw_ostream &O) {
+  const MCOperand &Op = MI->getOperand(OpNo);
+  if (Op.isReg()) {
+    printRegName(O, Op.getReg());
+    return;
+  }
+
+  if (Op.isImm()) {
+    O << Op.getImm();
+    return;
+  }
+
+  assert(Op.isExpr() && "unknown operand kind in printOperand");
+  Op.getExpr()->print(O, &MAI, true);
+}
+// @} MYRISCVXInstPrinter_cpp_printOperand
+
+
+void MYRISCVXInstPrinter::printUnsignedImm(const MCInst *MI, int opNum,
+                                           raw_ostream &O) {
+  const MCOperand &MO = MI->getOperand(opNum);
+  if (MO.isImm())
+    O << (unsigned short int)MO.getImm();
+  else
+    printOperand(MI, opNum, O);
+}
+
+// @{ MYRISCVXInstPrinter_cpp_printMemOperand
+void MYRISCVXInstPrinter::printMemOperand(const MCInst *MI, int opNum, raw_ostream &O) {
+  // Load/Store memory operands -- imm($reg)
+  // If PIC target the target is loaded as the
+  // pattern ld $t9,%call16($gp)
+  printOperand(MI, opNum+1, O);
+  O << "(";
+  printOperand(MI, opNum, O);
+  O << ")";
+}
+// @} MYRISCVXInstPrinter_cpp_printMemOperand
