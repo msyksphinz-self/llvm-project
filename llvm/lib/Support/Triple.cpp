4fed08b4f573161169357cda051a9699a6ea32d8
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-09-08 23:22:48 +0900

    Initial registration of MYRISCVX

--- llvm/lib/Support/Triple.cpp
+++ llvm/lib/Support/Triple.cpp
@@ -20,11 +20,13 @@
 #include <cstring>
 using namespace llvm;
 
+// @{ add_myriscvx32_64_into_triple_cpp
 StringRef Triple::getArchTypeName(ArchType Kind) {
   switch (Kind) {
   case UnknownArch:    return "unknown";
 
   case aarch64:        return "aarch64";
+  // @{ add_myriscvx32_64_into_triple_cpp ...
   case aarch64_32:     return "aarch64_32";
   case aarch64_be:     return "aarch64_be";
   case amdgcn:         return "amdgcn";
@@ -62,11 +64,15 @@ StringRef Triple::getArchTypeName(ArchType Kind) {
   case r600:           return "r600";
   case renderscript32: return "renderscript32";
   case renderscript64: return "renderscript64";
+  // @} add_myriscvx32_64_into_triple_cpp ...
   case riscv32:        return "riscv32";
   case riscv64:        return "riscv64";
+  case myriscvx32:     return "myriscvx32";   // myriscvx32(32-bit版MYRISCVX)を追加
+  case myriscvx64:     return "myriscvx64";   // myriscvx64(64-bit版MYRISCVX)を追加
   case shave:          return "shave";
   case sparc:          return "sparc";
   case sparcel:        return "sparcel";
+  // @} add_myriscvx32_64_into_triple_cpp
   case sparcv9:        return "sparcv9";
   case spir64:         return "spir64";
   case spir:           return "spir";
@@ -161,6 +167,9 @@ StringRef Triple::getArchTypePrefix(ArchType Kind) {
   case shave:       return "shave";
   case wasm32:
   case wasm64:      return "wasm";
+
+  case myriscvx32:
+  case myriscvx64:  return "myriscvx";
 
   case riscv32:
   case riscv64:     return "riscv";
@@ -333,6 +342,8 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("amdgcn", amdgcn)
     .Case("riscv32", riscv32)
     .Case("riscv64", riscv64)
+    .Case("myriscvx32", myriscvx32)
+    .Case("myriscvx64", myriscvx64)
     .Case("hexagon", hexagon)
     .Case("sparc", sparc)
     .Case("sparcel", sparcel)
@@ -477,6 +488,8 @@ static Triple::ArchType parseArch(StringRef ArchName) {
     .Case("amdgcn", Triple::amdgcn)
     .Case("riscv32", Triple::riscv32)
     .Case("riscv64", Triple::riscv64)
+    .Case("myriscvx32", Triple::myriscvx32)
+    .Case("myriscvx64", Triple::myriscvx64)
     .Case("hexagon", Triple::hexagon)
     .Cases("s390x", "systemz", Triple::systemz)
     .Case("sparc", Triple::sparc)
@@ -824,6 +837,8 @@ static Triple::ObjectFormatType getDefaultFormat(const Triple &T) {
   case Triple::renderscript64:
   case Triple::riscv32:
   case Triple::riscv64:
+  case Triple::myriscvx32:
+  case Triple::myriscvx64:
   case Triple::shave:
   case Triple::sparc:
   case Triple::sparcel:
@@ -1387,6 +1402,7 @@ static unsigned getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::hexagon:
   case llvm::Triple::hsail:
   case llvm::Triple::kalimba:
+  case llvm::Triple::myriscvx32:
   case llvm::Triple::lanai:
   case llvm::Triple::le32:
   case llvm::Triple::loongarch32:
@@ -1429,6 +1445,7 @@ static unsigned getArchPointerBitWidth(llvm::Triple::ArchType Arch) {
   case llvm::Triple::ppc64le:
   case llvm::Triple::renderscript64:
   case llvm::Triple::riscv64:
+  case llvm::Triple::myriscvx64:
   case llvm::Triple::sparcv9:
   case llvm::Triple::spir64:
   case llvm::Triple::spirv64:
@@ -1488,6 +1505,7 @@ Triple Triple::get32BitArchVariant() const {
   case Triple::ppcle:
   case Triple::r600:
   case Triple::renderscript32:
+  case Triple::myriscvx32:
   case Triple::riscv32:
   case Triple::shave:
   case Triple::sparc:
@@ -1528,6 +1546,7 @@ Triple Triple::get32BitArchVariant() const {
     break;
   case Triple::wasm64:         T.setArch(Triple::wasm32);  break;
   case Triple::x86_64:         T.setArch(Triple::x86);     break;
+  case Triple::myriscvx64:     T.setArch(Triple::myriscvx32); break;
   }
   return T;
 }
@@ -1570,6 +1589,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::ppc64le:
   case Triple::renderscript64:
   case Triple::riscv64:
+  case Triple::myriscvx64:
   case Triple::sparcv9:
   case Triple::spir64:
   case Triple::spirv64:
@@ -1593,6 +1613,7 @@ Triple Triple::get64BitArchVariant() const {
   case Triple::mipsel:
     T.setArch(Triple::mips64el, getSubArch());
     break;
+  case Triple::myriscvx32:      T.setArch(Triple::myriscvx64); break;
   case Triple::nvptx:           T.setArch(Triple::nvptx64);    break;
   case Triple::ppc:             T.setArch(Triple::ppc64);      break;
   case Triple::ppcle:           T.setArch(Triple::ppc64le);    break;
---
+++ llvm/lib/Target/MYRISCVX/MYRISCVX.h
@@ -0,0 +1,28 @@
+//===-- MYRISCVX.h - Top-level interface for MYRISCVX representation ----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in
+// the LLVM MYRISCVX back-end.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_MYRISCVX_MYRISCVX_H
+#define LLVM_LIB_TARGET_MYRISCVX_MYRISCVX_H
+
+#include "MCTargetDesc/MYRISCVXMCTargetDesc.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+  class MYRISCVXTargetMachine;
+  class FunctionPass;
+  class PassRegistry;
+
+} // end namespace llvm;
+
+#endif
