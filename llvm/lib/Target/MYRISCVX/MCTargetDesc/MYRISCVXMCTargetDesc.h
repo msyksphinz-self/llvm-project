7c0922a09a5a2087c664cf36ac6872dcc2c54f85
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-09-08 23:08:25 +0900

    Implementation of ObjectCodeEmitter

--- llvm/lib/Target/MYRISCVX/MCTargetDesc/MYRISCVXMCTargetDesc.h
+++ llvm/lib/Target/MYRISCVX/MCTargetDesc/MYRISCVXMCTargetDesc.h
@@ -15,18 +15,40 @@
 #define LLVM_LIB_TARGET_MYRISCVX_MCTARGETDESC_MYRISCVXMCTARGETDESC_H
 
 #include "llvm/Support/DataTypes.h"
+#include "llvm/MC/MCTargetOptions.h"
+#include <memory>
 
 namespace llvm {
+class MCAsmBackend;
+class MCCodeEmitter;
+class MCContext;
+class MCInstrInfo;
+class MCObjectTargetWriter;
+class MCRegisterInfo;
+class MCSubtargetInfo;
+class StringRef;
 class Target;
 class Triple;
+class raw_ostream;
+class raw_pwrite_stream;
 
 Target &getTheMYRISCVX32Target();
 Target &getTheMYRISCVX64Target();
 
+MCCodeEmitter *createMYRISCVXMCCodeEmitter(const MCInstrInfo &MCII,
+                                           MCContext &Ctx);
+
+MCAsmBackend *createMYRISCVXAsmBackend(const Target &T,
+                                       const MCSubtargetInfo &STI,
+                                       const MCRegisterInfo &MRI,
+                                       const MCTargetOptions &Options);
+
+std::unique_ptr<MCObjectTargetWriter> createMYRISCVXELFObjectWriter(const Triple &TT, bool Is64Bit);
+
 } // End llvm namespace
 
 // @{ MYRISCVXMC_TargetDesc_h_AddInclude
-// MYRISCVXGenRegisterInfo.inc / MYRISCVXGenInstrInfo.inc / 
+// MYRISCVXGenRegisterInfo.inc / MYRISCVXGenInstrInfo.inc /
 // MYRISCVXGenSubtargetInfo.inc からヘッダファイルに必要な情報を抽出する
 #define GET_REGINFO_ENUM
 #include "MYRISCVXGenRegisterInfo.inc"
